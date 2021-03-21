6d99a049a3f228cb972e5be7188dea227c9e2953
antonio.abreu <antonio.abreu@virtus.ufcg.edu.br>
2021-03-21 18:05:51 -0300

    Adicionando novo crud

---
+++ novo crud/crud/src/main/java/com/pedro/crud/data/vo/ProdutoVO.java
@@ -0,0 +1,38 @@
+package com.pedro.crud.data.vo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.pedro.crud.entity.Produto;
+import lombok.*;
+import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+
+@JsonPropertyOrder({"id","nome","estoque","preco"})
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class ProdutoVO extends RepresentationModel<ProdutoVO> implements Serializable {
+
+	private static final long serialVersionUID = 2938272643682548375L;
+
+	@JsonProperty("id")
+	private Long id;
+
+	@JsonProperty("nome")
+	private String nome;
+	
+	@JsonProperty("estoque")
+	private Integer estoque;
+	
+	@JsonProperty("preco")
+	private Double preco;
+	
+	public static ProdutoVO create(Produto produto) {
+		return new ModelMapper().map(produto, ProdutoVO.class);
+	}
+}
---
+++ novo crud/crud/src/main/java/com/pedro/crud/services/ProdutoService.java
@@ -0,0 +1,56 @@
+package com.pedro.crud.services;
+
+import com.pedro.crud.data.vo.ProdutoVO;
+import com.pedro.crud.entity.Produto;
+import com.pedro.crud.exception.ResourceNotFoundException;
+import com.pedro.crud.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+	private final ProdutoRepository produtoRepository;
+
+	@Autowired
+	public ProdutoService(ProdutoRepository produtoRepository) {
+		this.produtoRepository = produtoRepository;
+	}
+	
+	public ProdutoVO create(ProdutoVO produtoVO) {
+	 	ProdutoVO proVoRetorno = ProdutoVO.create(produtoRepository.save(Produto.create(produtoVO)));
+		return proVoRetorno;
+	}
+	
+	public Page<ProdutoVO> findAll(Pageable pageable) {
+		return produtoRepository.findAll(pageable).map(this::convertToProdutoVO);
+	}
+
+	private ProdutoVO convertToProdutoVO(Produto produto) {
+		return ProdutoVO.create(produto);
+	}
+
+	public ProdutoVO findById(Long id) {
+		return ProdutoVO.create(produtoRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID")));
+	}
+	
+	public ProdutoVO update(ProdutoVO produtoVO) {
+		final Optional<Produto> optionalProduto = produtoRepository.findById(produtoVO.getId());
+		
+		if(!optionalProduto.isPresent()) {
+			new ResourceNotFoundException("No records found for this ID");
+		}
+		
+		return ProdutoVO.create(produtoRepository.save(Produto.create(produtoVO)));
+	}
+	
+	public void delete(Long id) {
+		produtoRepository.delete(produtoRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID")));
+	}
+}
